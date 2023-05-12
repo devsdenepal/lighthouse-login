86ee812464dbe6d226636dd1a010e893aed5b563
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-05-13 03:21:38 +0545

    Add files via upload

--- lighthouse-login.py
+++ lighthouse-login.py
@@ -4,35 +4,28 @@ import requests
 import socket
 import os
 from bs4 import BeautifulSoup
+from modules.check_ip import return_ip
+from modules.fetch_input import fetch_input
+from colorama import init, Fore, Style
+
+# initialize colorama
+init()
+
 # ansi color vars
-RESET = "\033[0m"
-BOLD = "\033[1m"
-RED = "\033[31m"
-GREEN = "\033[32m"
-YELLOW = "\033[33m"
-BLUE = "\033[34m"
-MAGENTA = "\033[35m"
-CYAN = "\033[36m"
+RESET = Style.RESET_ALL
+BOLD = Style.BRIGHT
+RED = Fore.RED
+GREEN = Fore.GREEN
+YELLOW = Fore.YELLOW
+BLUE = Fore.BLUE
+MAGENTA = Fore.MAGENTA
+CYAN = Fore.CYAN
 
 socks.set_default_proxy(socks.SOCKS5, "localhost", 9050)
 socket.socket = socks.socksocket
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-url", help="URL of the website to fetch input names from")
 args = parser.parse_args()
-
-# Get the IP address of the user
-ip_address = requests.get('https://api.ipify.org').text
-print(f"{BOLD}Your IP Address:{RESET} {ip_address}\n")
-if not args.url:
-    print("Please provide a URL using the -url argument.")
-    exit()
-
-response = requests.get(args.url)
-soup = BeautifulSoup(response.content, 'html.parser')
-input_elements = soup.find_all('input')
-input_names = [elem.get('name') for elem in input_elements]
-
-print(f"{BOLD}Input Names:{RESET}")
-for name in input_names:
-    print(name)
+return_ip()
+print(f"{BLUE}Input Names:{RESET}")
+print(fetch_input(args.url))
